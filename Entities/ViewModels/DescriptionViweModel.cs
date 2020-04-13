f79a9cb75f758efa2e9c41c101bc1cb14254e972
IofceaCata <catalinalbert@gmail.com>
2020-04-13 19:47:12 +0300

    Structure and Repo update
    Structure and Repo update

---
+++ Entities/ViewModels/DescriptionViweModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatrimonialProject.ViewModels
+{
+    public class DescriptionViweModel
+    {
+        public string Content { get; set; }
+        public string Name { get; set; }
+    }
+}
